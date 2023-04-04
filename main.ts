1fea85463e12562aa7123a21e247440c4a10001f
muhammadv-i <76049040+muhammadv-i@users.noreply.github.com>
2023-04-04 14:59:15 +0000

    used 'registerEvent()' to ensure that resources are destroyed automatically

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ import { Plugin } from 'obsidian';
 
 export default class FrontmatterAliasDisplay extends Plugin {
 	displayAliases() {
-		const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
-		let files = fileExplorer.view.fileItems;
-		for (const file of Object.values(files)) {
+		// const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
+		// let files = fileExplorer.view.fileItems;
+		for (const file of Object.values(app.workspace.getLeavesOfType('file-explorer')[0].view.fileItems)) {
 			if (file.file) {
 
 				const aliasDiv = file.titleEl.querySelector('.file-alias')
@@ -21,11 +21,12 @@ export default class FrontmatterAliasDisplay extends Plugin {
 	
 	async onload() {
 		app.workspace.onLayoutReady(this.displayAliases);
-		app.workspace.on("layout-change", this.displayAliases);
-		app.workspace.on("editor-change", this.displayAliases);
+		this.registerEvent(app.workspace.on("layout-change", this.displayAliases));
+		this.registerEvent(app.workspace.on("editor-change", this.displayAliases));
 	}
 
 	onunload() {
+		app.workspace.off("editor-change", this.displayAliases);
 		const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
 		let files = fileExplorer.view.fileItems;
 		for (const file of Object.values(files)) {
