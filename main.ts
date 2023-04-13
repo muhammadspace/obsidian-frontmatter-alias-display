ded6de5d78b8e045c5f7d01e5f8db3b7663189db
Mohammad Magd <mohamed10.sw2021@fci.helwan.edu.eg>
2023-04-13 19:40:54 +0200

    alias vs aliases bugfix

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ import { Plugin } from 'obsidian';
 
 export default class FrontmatterAliasDisplay extends Plugin {
 	displayAliases() {
-		// const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
-		// let files = fileExplorer.view.fileItems;
-		for (const file of Object.values(app.workspace.getLeavesOfType('file-explorer')[0].view.fileItems)) {
+		const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
+		let files = fileExplorer.view.fileItems;
+		for (const file of Object.values(files)) {
 			if (file.file) {
 
 				const aliasDiv = file.titleEl.querySelector('.file-alias')
@@ -13,20 +13,22 @@ export default class FrontmatterAliasDisplay extends Plugin {
 				}
 
 				let aliases = app.metadataCache.getFileCache(file.file)?.frontmatter?.aliases;
+				let alias = app.metadataCache.getFileCache(file.file)?.frontmatter?.alias;
+
+                let aliasContent = alias ? alias : aliases;
 				// console.log(aliases);
-				file.titleEl.createEl('div', {text: aliases, cls: 'file-alias'});
+				file.titleEl.createEl('div', {text: aliasContent, cls: 'file-alias nav-file-title-content'});
 			}
 		}
 	}
 	
 	async onload() {
 		app.workspace.onLayoutReady(this.displayAliases);
-		this.registerEvent(app.workspace.on("layout-change", this.displayAliases));
-		this.registerEvent(app.workspace.on("editor-change", this.displayAliases));
+		app.workspace.on("layout-change", this.displayAliases);
+		app.workspace.on("editor-change", this.displayAliases);
 	}
 
 	onunload() {
-		app.workspace.off("editor-change", this.displayAliases);
 		const fileExplorer = app.workspace.getLeavesOfType('file-explorer')[0];
 		let files = fileExplorer.view.fileItems;
 		for (const file of Object.values(files)) {
